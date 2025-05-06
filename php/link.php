60230d667d128f7a240050fcea7ae80650ae58aa
OrtegaDev01 <eusouoortega@gmail.com>
2025-05-06 14:44:03 -0300

    versart0 1

---
+++ php/link.php
@@ -0,0 +1,23 @@
+<?php
+echo "
+    <a href='menu.php'>
+    Home
+    </a>
+    <a href='sobre.php'>
+    Sobre
+    </a>
+    <a href='vendas.php'>
+    venda
+    </a>
+    <a href='contatos.php'>
+    Contato
+    </a>
+    |
+    <a href='sair.php'>
+    Sair
+    </a>
+
+    
+
+    ";
+?>
